2da16bb03cacd21c703f5f607c263d7a6fd45872
Iryna Maslova <maslovai@hotmail.com>
2017-05-26 16:54:10 -0700

    deploying

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 
- page('/my-portfolio', myPortfolio.show);
+ page('/myportfolio', myPortfolio.show);
  page('/resume', myResume.show);
  page('/contact', myContact.show);
  page('/', pageView.initIndex);
